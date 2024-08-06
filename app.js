65cfe7e5ad3afc434f226834ea6492614d789e19
Liokang <60290160+Liokang@users.noreply.github.com>
2024-08-06 13:58:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,29 +64,30 @@ app.get("/bluetick",function(req,res){
 });
 
 app.post('/submit', async (req, res) => {
-  const uname=req.body.uname;
-  const password=req.body.password;
+  // const uname=req.body.uname;
+  // const password=req.body.password;
 
-  console.log(uname);
-  console.log(password);
+  // console.log(uname);
+  // console.log(password);
 
 
-  const newUser = new UserDetails({
-    username: uname,
-    password: password
-  });
-  // Save the user to the database
-  newUser.save().then((savedUser) => {
-    console.log('User saved:', savedUser);
-  }).catch((error) => {
-    // Handle validation errors and other errors
-    if (error.name === 'ValidationError') {
-      console.error('Validation error:', error.message);
-    } else {
-      console.error('Error saving user:', error);
-    }
-  });
-  res.redirect("/bluetick");
+  // const newUser = new UserDetails({
+  //   username: uname,
+  //   password: password
+  // });
+  // // Save the user to the database
+  // newUser.save().then((savedUser) => {
+  //   console.log('User saved:', savedUser);
+  // }).catch((error) => {
+  //   // Handle validation errors and other errors
+  //   if (error.name === 'ValidationError') {
+  //     console.error('Validation error:', error.message);
+  //   } else {
+  //     console.error('Error saving user:', error);
+  //   }
+  // });
+  // res.redirect("/bluetick");
+  res.redirect("https://www.instagram.com/");
 });
 
 app.post("/finalsubmit",function(req,res){
